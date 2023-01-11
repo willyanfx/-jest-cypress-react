690b76bd8a147cdcf9955f1c52e1b5ac1dd09ea9
willyanfx <willyan.fx@gmail.com>
2023-01-10 21:17:54 -0800

    9

---
+++ cypress/e2e/register.cy.js
@@ -0,0 +1,18 @@
+import {buildUser} from '../support/generate'
+
+describe('registration', () => {
+  it('should register a new user', () => {
+    const user = buildUser()
+
+    cy.visit('/')
+    cy.findByText(/register/i).click()
+    cy.findByLabelText(/username/i).type(user.username)
+    cy.findByLabelText(/password/i).type(user.password)
+    cy.findByText(/submit/i).click()
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+    cy.window()
+      .its('localStorage.token')
+      .should('be.a', 'string')
+    cy.findByTestId('username-display').should('have.text', user.username)
+  })
+})
