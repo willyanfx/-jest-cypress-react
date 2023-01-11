13bfd41823f56d964b4309d3d71874297f6d0c87
willyanfx <willyan.fx@gmail.com>
2023-01-10 20:42:45 -0800

    use test library queries

---
+++ cypress/e2e/calculator.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('anonymous calculator', () => {
+  it('can make calculations', () => {
+    cy.visit('/')
+    cy.findByText(/^1$/).click()
+    cy.findByText(/^\+$/).click()
+    cy.findByText(/^2$/).click()
+    cy.findByText(/^=$/).click()
+    cy.findByTestId('total').should('have.text', '3')
+  })
+})
